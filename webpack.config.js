5e76cb2a09c74384eb35707e7f0948362f870511
David whyte <kingstonwhyte87@gmail.com>
2019-03-27 11:04:25 +0100

    Setup react app

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlplugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+})
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlplugin]
+};
