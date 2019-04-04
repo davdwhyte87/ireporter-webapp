49000065c84060316772116fe5f899ad6c588b5c
David whyte <kingstonwhyte87@gmail.com>
2019-04-04 23:33:50 +0100

    feat: setup tests

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ class App extends React.Component {
           <Router>
             <Root>
               <Route exact path="/" component={Home}/>
+              {/* <Route path="/signup" component={} /> */}
             </Root>
           </Router>
         </div>
---
+++ src/test/Home.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from '../components/Home'
+import renderer from 'react-test-renderer'
+
+// test('Test for a home componenet', () => {
+//   const component = renderer.create(
+//     <Home/>
+//   )
+//   let tree = component.toJSON()
+//   expect(tree).toMatchSnapshot()
+// })
